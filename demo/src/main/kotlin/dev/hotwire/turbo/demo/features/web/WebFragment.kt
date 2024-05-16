42bfcca57e001e7143d4fb903db6f76bc842a9f9
James Hart <jhart@hellobrightline.com>
2024-05-16 12:40:31 -0700

    Update URL

--- demo/src/main/kotlin/dev/hotwire/turbo/demo/features/web/WebFragment.kt
+++ demo/src/main/kotlin/dev/hotwire/turbo/demo/features/web/WebFragment.kt
@@ -1,6 +1,9 @@
 package dev.hotwire.turbo.demo.features.web
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import dev.hotwire.strada.BridgeDelegate
 import dev.hotwire.turbo.demo.R
--- demo/src/main/kotlin/dev/hotwire/turbo/demo/util/Constants.kt
+++ demo/src/main/kotlin/dev/hotwire/turbo/demo/util/Constants.kt
@@ -1,6 +1,7 @@
 package dev.hotwire.turbo.demo.util
 
-const val BASE_URL = "https://turbo-native-demo.glitch.me"
+const val BASE_URL = "https://f9ef9755de6f.ngrok.app"
+
 const val HOME_URL = BASE_URL
 const val SIGN_IN_URL = "$BASE_URL/signin"
 const val NUMBERS_URL = "$BASE_URL/numbers"
